fa877511b6752e31d8ce17398189d996bb2af0cc
vupdivup <vnmatt2003@gmail.com>
2025-01-06 13:03:58 +0100

    add label renaming

--- main.js
+++ main.js
@@ -23,23 +23,67 @@ function addCard(group) {
     card.classList.add("card");
 
     // card label
-    let cardLabel = document.createElement("div");
-    cardLabel.classList.add("label");
-    cardLabel.innerText = "card";
-    card.appendChild(cardLabel);
+    let label = document.createElement("div");
+    label.classList.add("label");
+    label.innerText = "card";
+
+    card.appendChild(label);
+
+    // card label input
+    let renameInput = document.createElement("input");
+    renameInput.classList.add("label-input");
+    renameInput.style.display = "none";
+    card.appendChild(renameInput);
+
+    // initiate renaming via double click
+    label.addEventListener("dblclick", () => beginRename(label, renameInput));
+
+    // cancel rename if input is blurred
+    renameInput.addEventListener("blur", () => endRename(label, renameInput));
+
+    // rename hotkeys
+    renameInput.addEventListener("keydown", (e) => {
+        switch(e.code) {
+            // submit rename on Enter key press
+            case "Enter":
+                submitRename(label, renameInput);
+                break;
+            // blur if esc is pressed
+            case "Escape":
+                renameInput.blur();
+                break;
+        }
+    })
 
     // delete button
-    let deleteCardButton = document.createElement("button");
-    deleteCardButton.classList.add("delete-card");
-    deleteCardButton.innerText = "X";
-    deleteCardButton.addEventListener("click", () => card.remove());
-    card.appendChild(deleteCardButton);
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-card");
+    deleteButton.innerText = "X";
+    deleteButton.addEventListener("click", () => card.remove());
+    card.appendChild(deleteButton);
 
     // events for button hover visuals
-    card.addEventListener("mouseover", () => show(deleteCardButton));
-    card.addEventListener("mouseleave", () => hide(deleteCardButton));
+    card.addEventListener("mouseover", () => show(deleteButton));
+    card.addEventListener("mouseleave", () => hide(deleteButton));
 
     group.insertBefore(card, group.lastElementChild);
+}
+
+function beginRename(label, input) {
+    label.style.display = "none";
+    input.style.display = "block";
+    input.value = label.innerText;
+    input.focus();
+}
+
+function endRename(label, input) {
+    input.style.display = "none";
+    label.style.display = "block";
+}
+
+function submitRename(label, input) {
+    endRename(label, input);
+    label.innerText = input.value;
 }
 
 function show(element) {
