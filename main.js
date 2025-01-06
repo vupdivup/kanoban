b931e3059ecbea3e784244467b75dd09d3bb1459
vupdivup <vnmatt2003@gmail.com>
2025-01-06 10:38:28 +0100

    add card creation and basic group labels

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
-function addGroup() {
+function addGroup(groupName) {
     let board = document.getElementById("board");
 
     let group = document.createElement("div");
     group.classList.add("group");
 
-    board.insertBefore(group, this);
+    let groupLabel = document.createElement("div");
+    groupLabel.innerText = groupName;
+    group.appendChild(groupLabel);
+
+    let addCardButton = document.createElement("button");
+    addCardButton.innerText = "+";
+    addCardButton.addEventListener("click", () => addCard(group));
+    group.appendChild(addCardButton);
+
+    board.insertBefore(group, board.lastElementChild);
 }
 
-document.getElementById("add-group").addEventListener("click", addGroup);
+function addCard(group) {
+    let card = document.createElement("div");
+    card.innerText = "card";
+    group.insertBefore(card, group.lastElementChild);
+}
+
+document.getElementById("add-group").addEventListener("click", () => addGroup("group"));
+addGroup("group");
